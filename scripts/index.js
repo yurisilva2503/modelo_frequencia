d0c6e7331c3e6d87470a998fc36eb776e9ef770b
yurisilva2503 <yurigsfer@gmail.com>
2024-05-02 23:11:36 -0300

    feat: Added initial logic for aligning lines based on month days; updated PDF printing and interface.

--- scripts/index.js
+++ scripts/index.js
@@ -1,110 +1,60 @@
+// Variaveis
+const primary_table = document.getElementById(
+  "primary_table-container"
+).innerHTML;
+const secundary_table = document.getElementById(
+  "secundary_table-container"
+).innerHTML;
+const wrapper = document.getElementById("wrapper");
+const btn = document.getElementById("btn-impress");
+const footer = document.getElementById("footer");
+const date_container = document.getElementById("date-container");
+const signatures = document.getElementById("signatures");
+const address = document.getElementById("address");
+const currentYear = new Date().getFullYear();
+
+// Funções
+const fillMonthTable = (month) => {
+  const daysInMonth = new Date(currentYear, month, 0).getDate();
+  for (let i = 1; i <= daysInMonth; i++) {
+    const row = `
+      <tr>
+        <td class="cel-secundary_table -day">${i}</td>
+        <td class="cel-secundary_table -rubric"></td>
+        <td class="cel-secundary_table -entrytime"></td>
+        <td class="cel-secundary_table -exittime"></td>
+        <td class="cel-secundary_table -entrytime"></td>
+        <td class="cel-secundary_table -exittime"></td>
+        <td class="cel-secundary_table -occurrences"></td>
+        <td class="cel-secundary_table -observations"></td>
+      </tr>
+    `;
+    document
+      .getElementById("secundary_table")
+      .insertAdjacentHTML("beforeend", row);
+  }
+
+  if (daysInMonth < 30) {
+    date_container.style.marginTop = "2%";
+    signatures.style.marginTop = "4%";
+    address.style.marginTop = "6.5%";
+  } else if (daysInMonth == 30) {
+    date_container.style.marginTop = "2%";
+    signatures.style.marginTop = "3%";
+    address.style.marginTop = "4%";
+  } else {
+    date_container.style.marginTop = "2%";
+    signatures.style.marginTop = "2%";
+    address.style.marginTop = "3%";
+  }
+  
+}
+
 const makePDF = () => {
-  var primary_table = document.getElementById("primary_table-container").innerHTML;
-  var secundary_table = document.getElementById("secundary_table-container").innerHTML;
+  btn.style.display = "none";
+  wrapper.style.width = "93%";
+  window.print();
 
-  var style = `
-  <style>
-
-    * {
-    border: 0px;
-    padding: 0px;
-    box-sizing: border-box;
-    font-family: Arial;
-    }
-
-    body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #fff;
-    }
-
-    .wrapper{
-    width: 60vw;
-    }
-
-    #primary_table-container {
-    width: 100%;
-    }
-
-    #primary_table {
-    border-collapse: collapse;
-    width: 100%;
-    font-weight: bold;
-    font-size: 1em;
-    }
-
-    .table-caption {
-    margin-bottom: 6px;
-    }
-
-    .main-logo {
-    width: 650px;
-    height: 90px;
-    }
-
-    .sector{
-    border: 2px solid #000;
-    padding: 1px;
-    }
-
-
-    tbody > tr:nth-child(1) > td{
-    padding-top: 2.5px;
-    text-align: center;
-    }
-
-
-    #secundary_table-container {
-    width: 100%;
-    }
-
-
-    #secundary_table {
-    margin-top: 6px;
-    border-collapse: collapse;
-    width: 100%;
-    font-weight: bold;
-    font-size: 1em;
-    }
-
-    .cel-secundary_table{
-    border: 2px solid #000;
-    }
-
-    #secundary_table > thead > tr > th:nth-child(1){
-    width: 5%
-    }
-    #secundary_table > thead > tr > th:nth-child(2){
-    width: 10%
-    }
-    #secundary_table > thead > tr > th:nth-child(3){
-    width: 32.5%
-    }
-    #secundary_table > thead > tr > th:nth-child(4){
-    width: 32.5%
-    }
-    #secundary_table > thead > tr > th:nth-child(5){
-    width: 8%
-    }
-    #secundary_table > thead > tr > th:nth-child(6){
-    width: 12%
-    }
-    
-    </style>
-  `;
-
-  var win = window.open("", "", "");
-
-  win.document.write("<html><head>");
-  win.document.write("<title>Frequência</title>");
-  win.document.write(style);
-  win.document.write("</head>");
-  win.document.write("<body>");
-  win.document.write(primary_table);
-  win.document.write(secundary_table);
-  win.document.write("</body></html>");
-
-  win.document.close();
-  win.print();
+  btn.style.display = "block";
+  wrapper.style.width = "55%";
 };
