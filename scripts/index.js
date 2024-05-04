b5383f9592618613e4959526dfad5dce1dcb6442
yurisilva2503 <yurigsfer@gmail.com>
2024-05-04 08:13:43 -0300

    fix: refactoring the code for the same html page

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,16 @@
-// Variaveis
-const primary_table = document.getElementById(
-  "primary_table-container"
-).innerHTML;
-const secundary_table = document.getElementById(
-  "secundary_table-container"
-).innerHTML;
-const wrapper = document.getElementById("wrapper");
-const btn = document.getElementById("btn-impress");
-const date_container = document.getElementById("date-container");
-const signatures = document.getElementById("signatures");
-const address = document.getElementById("address");
+const impress_primary_table = document.getElementById("impress_primary-table-container").innerHTML;
+const impress_secundary_table = document.getElementById("impress_secondary-table-container").innerHTML;
+const impress_wrapper = document.getElementById("impress_wrapper");
+const impress_btn = document.getElementById("impress_btn-impress");
+const impress_date_container = document.getElementById("impress_date-container");
+const impress_signatures = document.getElementById("impress_signatures");
+const impress_address = document.getElementById("impress_address");
 const currentYear = new Date().getFullYear();
 
-// Funções
 const fillMonthTable = (month) => {
   const daysInMonth = new Date(currentYear, month, 0).getDate();
-  const dayOfWeekCellContent = (isSaturday, isSunday) =>
-    isSaturday ? "Sábado" : isSunday ? "Domingo" : "";
-  const weekendClass = (isSaturday, isSunday) =>
-    isSaturday || isSunday ? "weekend" : "";
+  const dayOfWeekCellContent = (isSaturday, isSunday) => isSaturday ? "Sábado" : isSunday ? "Domingo" : "";
+  const weekendClass = (isSaturday, isSunday) => isSaturday || isSunday ? "weekend" : "";
 
   for (let i = 1; i <= daysInMonth; i++) {
     const currentDate = new Date(currentYear, month - 1, i);
@@ -29,41 +21,41 @@ const fillMonthTable = (month) => {
 
     const row = `
       <tr>
-        <td class="cel-secundary_table -day ${cellClass}">${i}</td>
-        <td class="cel-secundary_table -rubric ${cellClass}">${cellContent}</td>
-        <td class="cel-secundary_table -entrytime ${cellClass}">${cellContent}</td>
-        <td class="cel-secundary_table -exittime ${cellClass}">${cellContent}</td>
-        <td class="cel-secundary_table -entrytime ${cellClass}">${cellContent}</td>
-        <td class="cel-secundary_table -exittime ${cellClass}">${cellContent}</td>
-        <td class="cel-secundary_table -occurrences ${cellClass}">${cellContent}</td>
-        <td class="cel-secundary_table -observations ${cellClass}">${cellContent}</td>
+        <td class="impress_cel-secondary-table -day ${cellClass}">${i}</td>
+        <td class="impress_cel-secondary-table -rubric ${cellClass}">${cellContent}</td>
+        <td class="impress_cel-secondary-table -entrytime ${cellClass}">${cellContent}</td>
+        <td class="impress_cel-secondary-table -exittime ${cellClass}">${cellContent}</td>
+        <td class="impress_cel-secondary-table -entrytime ${cellClass}">${cellContent}</td>
+        <td class="impress_cel-secondary-table -exittime ${cellClass}">${cellContent}</td>
+        <td class="impress_cel-secondary-table -occurrences ${cellClass}">${cellContent}</td>
+        <td class="impress_cel-secondary-table -observations ${cellClass}">${cellContent}</td>
       </tr>
     `;
     document
-      .getElementById("secundary_table")
+      .getElementById("impress_secondary-table")
       .insertAdjacentHTML("beforeend", row);
   }
 
   if (daysInMonth < 30) {
-    date_container.style.marginTop = "2%";
-    signatures.style.marginTop = "4%";
-    address.style.marginTop = "6.5%";
+    impress_date_container.style.marginTop = "2%";
+    impress_signatures.style.marginTop = "4%";
+    impress_address.style.marginTop = "6.5%";
   } else if (daysInMonth == 30) {
-    date_container.style.marginTop = "2%";
-    signatures.style.marginTop = "3%";
-    address.style.marginTop = "4%";
+    impress_date_container.style.marginTop = "2%";
+    impress_signatures.style.marginTop = "3%";
+    impress_address.style.marginTop = "4%";
   } else {
-    date_container.style.marginTop = "2%";
-    signatures.style.marginTop = "2%";
-    address.style.marginTop = "3%";
+    impress_date_container.style.marginTop = "2%";
+    impress_signatures.style.marginTop = "2%";
+    impress_address.style.marginTop = "3%";
   }
 };
 
 const makePDF = () => {
-  btn.style.display = "none";
-  wrapper.style.width = "93%";
+  impress_btn.style.display = "none";
+  impress_wrapper.style.width = "93%";
   window.print();
 
-  btn.style.display = "block";
-  wrapper.style.width = "50%";
+  impress_btn.style.display = "block";
+  impress_wrapper.style.width = "50%";
 };
