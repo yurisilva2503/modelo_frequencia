966d9b5c92a4e8b78459a840edd90a1576f4e7b8
yurisilva2503 <yurigsfer@gmail.com>
2024-05-07 21:50:16 -0300

    release: test version

--- scripts/index.js
+++ scripts/index.js
@@ -83,140 +83,84 @@ const monthsofyear = [
   }
 ]
 
-const sectors = [
-  {
-    id: "Almoxarifado",
-    name: "Almoxarifado",
-  },
-  {
-    id: "ASCOM",
-    name: "Assessoria de Comunicação - ASCOM",
-  },
-  {
-    id: "Assessoria DCTV",
-    name: "Assessoria DCTV",
-  },
-  {
-    id: "Assessoria DETEC",
-    name: "Assessoria DETEC",
-  },
-  {
-    id: "CAEI",
-    name: "Coordenadoria de Apoio ao Empreendedorismo e a Inovação - CAEI",
-  },
-  {
-    id: "CCTI",
-    name: "Coordenadoria de Ciência e Tecnologia - CCTI",
-  },
-  {
-    id: "COEAD",
-    name: "Coordenadoria de Educação à Distância - COEAD",
-  },
-  {
-    id: "COEDS",
-    name: "Coordenadoria de Educação Superior - COEDS",
-  },
-  {
-    id: "COESC",
-    name: "Coordenadoria de Serviços nas Unidades Escolares - COESC",
-  },
-  {
-    id: "COFIC",
-    name: "Coordenadoria de Formação Inicial e Continuada - COFIC",
-  },
-  {
-    id: "COLOP",
-    name: "Coordenadoria de Logística e Patrimônio - COLOP",
-  },
-  {
-    id: "COMSER",
-    name: "Coordenadoria de Compras e Serviços - COMSER",
-  },
-  {
-    id: "CONFIN",
-    name: "Coordenadoria de Orçamento e Finanças - CONFIN",
-  },
-  {
-    id: "CETEC",
-    name: "Coordenadoria de Educação Técnica e Tecnológica - CETEC",
-  },
-  {
-    id: "CGEPES",
-    name: "Coordenadoria de Gestão de Pessoas - CGEPES",
-  },
-  {
-    id: "CTDS",
-    name: "Coordenadoria de Tecnologia para o Desenvolvimento Social - CTDS",
-  },
-  {
-    id: "DAF",
-    name: "Diretoria de Administração e Finanças - DAF",
-  },
-  {
-    id: "DCT",
-    name: "Diretoria de Ciência e tecnologia - DCT",
-  },
-  {
-    id: "DETEC",
-    name: "Anexo-Diretoria de Educação Profissional e Tecnológica - DETEC",
-  },
-  {
-    id: "EETEPAS",
-    name: "Setor Sociopsicopedagógico - EETEPAS",
-  },
-  {
-    id: "GECONC",
-    name: "Gerência de Contratos e Convênio - GECONC",
-  },
-  {
-    id: "GEPC",
-    name: "Gerência de Prestação de Contas - GEPC",
-  },
-  {
-    id: "GETIC",
-    name: "Gerência de Tecnologia da Informação e Comunicação - GETIC",
-  },
-  {
-    id: "Gerência Financeira (PRONATEC)",
-    name: "Gerência Financeira (PRONATEC)",
-  },
-  {
-    id: "Gabinete",
-    name: "Gabinete",
-  },
-  {
-    id: "NUCAD",
-    name: "Núcleo Consultivo da Administração Direta - NUCAD",
-  },
-  {
-    id: "NUENG",
-    name: "Núcleo de Obras e Serviços de Engenharia - NUENG",
-  },
-  {
-    id: "NUPLAN",
-    name: "Núcleo de Planejamento - NUPLAN",
-  },
-  {
-    id: "NUCI",
-    name: "Controle Interno - NUCI",
-  },
-  {
-    id: "Procurador do Estado",
-    name: "Procurador do Estado",
-  },
-  {
-    id: "SECAD",
-    name: "Secretaria Adjunta - SECAD",
-  },
-  {
-    id: "Sala da Diretoria",
-    name: "Sala da Diretoria",
-  },
-  {
-    id: "Sala do Secretário",
-    name: "Sala do Secretário",
-  },
+const directorates = [
+  { id: "DAF", name: "Diretoria de Administração e Finanças - DAF" },
+  { id: "DCOME", name: "Diretoria de Apoio de Comércio Exterior" },
+  { id: "DCT", name: "Diretoria de Ciência e Tecnologia - DCT" },
+  { id: "DETEC", name: "Anexo-Diretoria de Educação Profissional e Tecnológica - DETEC" },
+  { id: "DITT", name: "Diretoria de Inovação e Transferência de Tecnologia - DITT" },
 ]
+
+const sectors = [
+    { id: "ASCOM", name: "Assessoria de Comunicação" },
+    { id: "ASDETEC", name: "Assessoria da Diretoria de Educação Profissional e Tecnológica" },
+    { id: "ASGAB", name: "Assessoria de Gabinete" },
+    { id: "CAEI", name: "Coordenadoria de Apoio ao Empreendedorismo e à Inovação - CAEI" },
+    { id: "CCT", name: "Coordenadoria de Ciência e Tecnologia - CCTI" },
+    { id: "CETEC", name: "Coordenadoria de Edicação Técnica e Tecnológica - CETEC" },
+    { id: "COAD", name: "Coordenadoria Administrativa - COAD" },
+    { id: "COEAD", name: "Coordenadoria de Educação à Distância - COEAD" },
+    { id: "COEDS", name: "Coordenadoria de Educação Superior - COEDS" },
+    { id: "COESC", name: "Coordenadoria de Serviços nas Unidades Escolares - COESC" },
+    { id: "COFIC", name: "Coordenadoria de Formação Inicial e Continuada - COFIC" },
+    { id: "COFIN", name: "Coordenadoria de Orçamento e Finanças - CONFIN" },
+    { id: "COLOP", name: "Coordenadoria de Logística e Patrimônio - COLOP" },
+    { id: "COMSER", name: "Coordenadoria de Compras e Serviços - COMSER" },
+    { id: "CONS", name: "Conselho Estadual de Ciência, Tecnologia e Educação Profissional e Tecnológica - CONS" },
+    { id: "COPS", name: "Coordenadoria do Programa Startup Pará - COPS" },
+    { id: "COSAN", name: "Comissão Sancionatória - COSASN" },
+    { id: "CPCCR", name: "Comissão Plano de Cargos, Carreira e Remuneração - CPCCR" },
+    { id: "CGEPES", name: "Coordenadoria de Gestão de Pessoas - CGEPES" },
+    { id: "CTDS", name: "Coordenadoria de Tecnologia para o Desenvolvimento Social - CTDS" },
+    { id: "CPL", name: "Comissão Permanente de Licitação - CPL" },
+    { id: "CTPAZ", name: "Coordenadoria de Apoio ao Ter Paz - CTPAZ" },
+    { id: "Gabinete", name: "Gabinete" },
+    { id: "GAEI", name: "Gerência de Apoio ao Empreendedorismo e à Inovação - GAEI" },
+    { id: "GCT", name: "Gerência de Ciência e Tecnologia - GCT" },
+    { id: "GEAD", name: "Gerência de Cursos à Distância - GEAD" },
+    { id: "GEAPL", name: "Gerência APLS - GEAPL" },
+    { id: "GECOM", name: "Gerência de Competitividade: GECOM" },
+    { id: "GECONT", name: "Gerência de Contratos - GECONT" },
+    { id: "GECONV", name: "Gerência de Convênios e Instrumentos - GECONV" },
+    { id: "GEDIP", name: "Gerência de Diversificação Produtiva - GEDIP" },
+    { id: "GEFIC", name: "Gerência de Acreditação de Cursos de FIC - GEFIC" },
+    { id: "GEFIN", name: "Gerência de Execução Financeira - GEFIN" },
+    { id: "GEIFO", name: "Gerência de Incentivos e Fomentos - GEFIN" },
+    { id: "GELOG", name: "Gerência de Logística e Infraestrutura - GELOG" },
+    { id: "GEMER", name: "Gerência de Estudos de Mercado - GEMER" },
+    { id: "GEMIC", name: "Gerência de Micro Empresa - GEMIC" },
+    { id: "GEMON", name: "Gerência de Monitoramento e Estatística - GEMON" },
+    { id: "GEORC", name: "Gerência de Execução Orçamentária - GEORC" },
+    { id: "GEPAM", name: "Gerência de Patrimônio e Material - GEPAM" },
+    { id: "GEPC", name: "Gerência de Prestação de Contas - GEPC" },
+    { id: "GEPES", name: "Gerência de Pessoal - GEPES" },
+    { id: "GEPEV", name: "Gerência de Promoção de Eventos - GEPEV" },
+    { id: "GEPOR", name: "Gerência de Planejamento e Orçamento - GEPOR" },
+    { id: "GETEC", name: "Gerência de Cursos Técnicos e Tecnológicos - GETEC" },
+    { id: "GETIC", name: "Gerência de Tecnologia da Informação e Comunicação - GETIC" },
+    { id: "GEVAPES", name: "Gerência de Valorização de Pessoas - GEVAPES" },
+    { id: "GFDIA", name: "Gerência Financeira Diária - GFDIA" },
+    { id: "GFPAG", name: "Gerência Financeira Pagamentos - GFPAG" },
+    { id: "GTDS", name: "Gerência de Tecnologias para o Desenvolvimento Social - GTDS" },
+    { id: "INFOC", name: "Coordenadoria de Infocentro - INFOC" },
+    { id: "MICRO", name: "Coordenadoria de Micros e Pequenas Empresas - MICRO" },
+    { id: "NUCI", name: "Controle Interno - NUCI" },
+    { id: "NUENG", name: "Núcleo de Obras e Serviços de Engenharia - NUENG" },
+    { id: "NUJUR", name: "Núcleo Jurídico - NUJUR" },
+    { id: "NUPLAN", name: "Núcleo de Planejamento - NUPLAN" },
+    { id: "NURI", name: "Núcleo de Relações Interinstitucionais - NURI" },
+    { id: "PASS", name: "Solicitação de Passagens - PASS" },
+    { id: "PESQU", name: "Coordenadoria de Redes de Pesquisa - PESQU" },
+    { id: "PROC", name: "Procurador-Chefe - PROC" },
+    { id: "PROJ", name: "Coordenadoria de Projetos Estratégicos - PROJ" },
+    { id: "PROTO", name: "Protocolo Geral - PROTO" },
+    { id: "SEC", name: "Secretário - SEC" },
+    { id: "SIC", name: "Serviço de Informação ao Cidadão - SIC" },
+    { id: "SPP", name: "Sociopsicopedagógico" },
+    { id: "SECAD", name: "Secretaria Adjunta - SECAD" }
+]
+
+const concatenated_DirectoratesSectors = sectors.slice(0, 22).concat(directorates.slice(0)).concat(sectors.slice(22));
 
 const holidays = [
   { day: 1, month: 1, holiday: "Feriado Nacional - Confraternização Universal" },
@@ -254,15 +198,31 @@ const workloads = [
   }
 ]
 
+const handleGetDirectorates = () => {
+  directorates.forEach((directorate) => {
+    const row = `
+      <option value='${directorate.id}'>${directorate.id}</option>
+    `
+    index_select_sector_primary.innerHTML += row
+  })
+}
+
 const handleGetSectors = () => {
   sectors.forEach((sector) => {
     const row = `
       <option value='${sector.id}'>${sector.id}</option>
     `
-    index_select_sector_primary.innerHTML += row
     index_select_sector_secondary.innerHTML += row
-    index_select_location.innerHTML += row
   })
+}
+
+const handleGetConcatDirectoratesSectors = () => {
+  concatenated_DirectoratesSectors.forEach((value) => {
+    const row = `
+      <option value='${value.id}'>${value.id}</option>
+    `;
+    index_select_location.innerHTML += row;
+  });
 }
 
 const handleGetMonthsOfYear = () => {
@@ -287,7 +247,9 @@ document.addEventListener("DOMContentLoaded", () => {
   stringReplace(index_input_name)
   stringReplace(index_input_role)
   numberReplace(index_input_registration)
+  handleGetDirectorates()
   handleGetSectors()
+  handleGetConcatDirectoratesSectors()
   handleGetMonthsOfYear()
   handleGetWorkloads()
 })
@@ -308,10 +270,14 @@ function numberReplace(num) {
   })
 }
 
+function hasNumber(str) {
+  return /\d/.test(str);
+}
+
 function isEmptyOrNull(fields) {
   return fields.some(
     (value) =>
-      !value || value.trim() === "" || value.trim().toLowerCase() === "null"
+      !value || value.trim() == "" || value.trim().toLowerCase() == "null"
   )
 }
 
@@ -366,6 +332,21 @@ function generateUser() {
 
   if(user.registration.length <= 6 || user.registration.length >= 11){
     alert("O campo de matrícula deve ter no mínimo 7 e no máximo 10 caracteres.")
+    return
+  }
+
+  if (hasNumber(user.name) == true) {
+    alert("O campo de nome deve conter apenas letras.")
+    return
+  }
+
+  if (user.registration.includes("/")) {
+    if (user.registration.split("/")[1].length != 1) {
+      alert("O campo de matrícula deve ter apenas um número após a barra.")
+      return
+    }
+  } else if (user.registration.length > 8) {
+    alert("Números de matrícula sem a barra devem conter no máximo 8 caracteres.")
     return
   }
 
@@ -441,9 +422,9 @@ function fillMonthTable(user) {
     impress_address.style.marginTop = "3%"
   }
 
-  const primary_sectorText = sectors.find((sectorFilter) => sectorFilter.id == user.primary_sector);
-  const secondary_sectorText = sectors.find((sectorFilter) => sectorFilter.id == user.secondary_sector);
-  const monthText = monthsofyear.find((monthFilter) => monthFilter.id == user.month)
+  const primary_sectorText = directorates.find((directorate) => directorate.id == user.primary_sector);
+  const secondary_sectorText = sectors.find((sector) => sector.id == user.secondary_sector);
+  const monthText = monthsofyear.find((month) => month.id == user.month)
   
   if (secondary_sectorText) {
     impress_secondary_sector.style.display = "table-cell"
