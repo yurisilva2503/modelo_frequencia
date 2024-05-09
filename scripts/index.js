7e5646c2290fd4edee9c18fb9a5a8afe2a1317cf
yurisilva2503 <yurigsfer@gmail.com>
2024-05-09 12:50:20 -0300

    fix: sectors update

--- scripts/index.js
+++ scripts/index.js
@@ -85,10 +85,10 @@ const monthsofyear = [
 
 const directorates = [
   { id: "DAF", name: "Diretoria de Administração e Finanças - DAF" },
-  { id: "DCOME", name: "Diretoria de Apoio de Comércio Exterior" },
   { id: "DCT", name: "Diretoria de Ciência e Tecnologia - DCT" },
   { id: "DETEC", name: "Anexo-Diretoria de Educação Profissional e Tecnológica - DETEC" },
-  { id: "DITT", name: "Diretoria de Inovação e Transferência de Tecnologia - DITT" },
+  { id: "Gabinete", name: "Gabinete" },
+  { id: "SECAD", name: "Secretaria Adjunta - SECAD" }
 ]
 
 const sectors = [
@@ -98,7 +98,6 @@ const sectors = [
     { id: "CAEI", name: "Coordenadoria de Apoio ao Empreendedorismo e à Inovação - CAEI" },
     { id: "CCT", name: "Coordenadoria de Ciência e Tecnologia - CCTI" },
     { id: "CETEC", name: "Coordenadoria de Edicação Técnica e Tecnológica - CETEC" },
-    { id: "COAD", name: "Coordenadoria Administrativa - COAD" },
     { id: "COEAD", name: "Coordenadoria de Educação à Distância - COEAD" },
     { id: "COEDS", name: "Coordenadoria de Educação Superior - COEDS" },
     { id: "COESC", name: "Coordenadoria de Serviços nas Unidades Escolares - COESC" },
@@ -106,19 +105,15 @@ const sectors = [
     { id: "COFIN", name: "Coordenadoria de Orçamento e Finanças - CONFIN" },
     { id: "COLOP", name: "Coordenadoria de Logística e Patrimônio - COLOP" },
     { id: "COMSER", name: "Coordenadoria de Compras e Serviços - COMSER" },
-    { id: "CONS", name: "Conselho Estadual de Ciência, Tecnologia e Educação Profissional e Tecnológica - CONS" },
-    { id: "COPS", name: "Coordenadoria do Programa Startup Pará - COPS" },
     { id: "COSAN", name: "Comissão Sancionatória - COSASN" },
     { id: "CPCCR", name: "Comissão Plano de Cargos, Carreira e Remuneração - CPCCR" },
     { id: "CGEPES", name: "Coordenadoria de Gestão de Pessoas - CGEPES" },
     { id: "CTDS", name: "Coordenadoria de Tecnologia para o Desenvolvimento Social - CTDS" },
     { id: "CPL", name: "Comissão Permanente de Licitação - CPL" },
     { id: "CTPAZ", name: "Coordenadoria de Apoio ao Ter Paz - CTPAZ" },
-    { id: "Gabinete", name: "Gabinete" },
     { id: "GAEI", name: "Gerência de Apoio ao Empreendedorismo e à Inovação - GAEI" },
     { id: "GCT", name: "Gerência de Ciência e Tecnologia - GCT" },
     { id: "GEAD", name: "Gerência de Cursos à Distância - GEAD" },
-    { id: "GEAPL", name: "Gerência APLS - GEAPL" },
     { id: "GECOM", name: "Gerência de Competitividade: GECOM" },
     { id: "GECONT", name: "Gerência de Contratos - GECONT" },
     { id: "GECONV", name: "Gerência de Convênios e Instrumentos - GECONV" },
@@ -127,14 +122,10 @@ const sectors = [
     { id: "GEFIN", name: "Gerência de Execução Financeira - GEFIN" },
     { id: "GEIFO", name: "Gerência de Incentivos e Fomentos - GEFIN" },
     { id: "GELOG", name: "Gerência de Logística e Infraestrutura - GELOG" },
-    { id: "GEMER", name: "Gerência de Estudos de Mercado - GEMER" },
-    { id: "GEMIC", name: "Gerência de Micro Empresa - GEMIC" },
-    { id: "GEMON", name: "Gerência de Monitoramento e Estatística - GEMON" },
     { id: "GEORC", name: "Gerência de Execução Orçamentária - GEORC" },
     { id: "GEPAM", name: "Gerência de Patrimônio e Material - GEPAM" },
     { id: "GEPC", name: "Gerência de Prestação de Contas - GEPC" },
     { id: "GEPES", name: "Gerência de Pessoal - GEPES" },
-    { id: "GEPEV", name: "Gerência de Promoção de Eventos - GEPEV" },
     { id: "GEPOR", name: "Gerência de Planejamento e Orçamento - GEPOR" },
     { id: "GETEC", name: "Gerência de Cursos Técnicos e Tecnológicos - GETEC" },
     { id: "GETIC", name: "Gerência de Tecnologia da Informação e Comunicação - GETIC" },
@@ -142,25 +133,14 @@ const sectors = [
     { id: "GFDIA", name: "Gerência Financeira Diária - GFDIA" },
     { id: "GFPAG", name: "Gerência Financeira Pagamentos - GFPAG" },
     { id: "GTDS", name: "Gerência de Tecnologias para o Desenvolvimento Social - GTDS" },
-    { id: "INFOC", name: "Coordenadoria de Infocentro - INFOC" },
-    { id: "MICRO", name: "Coordenadoria de Micros e Pequenas Empresas - MICRO" },
     { id: "NUCI", name: "Controle Interno - NUCI" },
     { id: "NUENG", name: "Núcleo de Obras e Serviços de Engenharia - NUENG" },
     { id: "NUJUR", name: "Núcleo Jurídico - NUJUR" },
     { id: "NUPLAN", name: "Núcleo de Planejamento - NUPLAN" },
-    { id: "NURI", name: "Núcleo de Relações Interinstitucionais - NURI" },
-    { id: "PASS", name: "Solicitação de Passagens - PASS" },
-    { id: "PESQU", name: "Coordenadoria de Redes de Pesquisa - PESQU" },
-    { id: "PROC", name: "Procurador-Chefe - PROC" },
-    { id: "PROJ", name: "Coordenadoria de Projetos Estratégicos - PROJ" },
-    { id: "PROTO", name: "Protocolo Geral - PROTO" },
-    { id: "SEC", name: "Secretário - SEC" },
-    { id: "SIC", name: "Serviço de Informação ao Cidadão - SIC" },
-    { id: "SPP", name: "Sociopsicopedagógico" },
-    { id: "SECAD", name: "Secretaria Adjunta - SECAD" }
+    { id: "PROTOCOLO", name: "Protocolo Geral - PROTO" }
 ]
 
-const concatenated_DirectoratesSectors = sectors.slice(0, 22).concat(directorates.slice(0)).concat(sectors.slice(22));
+const concatenated_DirectoratesSectors = sectors.slice(0, 22).concat(directorates.slice(0)).concat(sectors.slice(22))
 
 const holidays = [
   { day: 1, month: 1, holiday: "Feriado Nacional - Confraternização Universal" },
@@ -180,7 +160,7 @@ const holidays = [
   { day: 20, month: 11, holiday: "Feriado Nacional - Dia Nacional de Zumbi e da Consciência Negra" },
   { day: 24, month: 12, holiday: "Ponto Facultativo - Véspera de Natal" },
   { day: 25, month: 12, holiday: "Feriado Nacional - Natal" },
-  { day: 31, month: 12, holiday: "Ponto Facultativo - Véspera de Ano Novo" },
+  { day: 31, month: 12, holiday: "Ponto Facultativo - Véspera de Ano Novo" }
 ]
 
 const workloads = [
@@ -220,9 +200,9 @@ const handleGetConcatDirectoratesSectors = () => {
   concatenated_DirectoratesSectors.forEach((value) => {
     const row = `
       <option value='${value.id}'>${value.id}</option>
-    `;
-    index_select_location.innerHTML += row;
-  });
+    `
+    index_select_location.innerHTML += row
+  })
 }
 
 const handleGetMonthsOfYear = () => {
@@ -271,7 +251,7 @@ function numberReplace(num) {
 }
 
 function hasNumber(str) {
-  return /\d/.test(str);
+  return /\d/.test(str)
 }
 
 function isEmptyOrNull(fields) {
@@ -422,8 +402,8 @@ function fillMonthTable(user) {
     impress_address.style.marginTop = "3%"
   }
 
-  const primary_sectorText = directorates.find((directorate) => directorate.id == user.primary_sector);
-  const secondary_sectorText = sectors.find((sector) => sector.id == user.secondary_sector);
+  const primary_sectorText = directorates.find((directorate) => directorate.id == user.primary_sector)
+  const secondary_sectorText = sectors.find((sector) => sector.id == user.secondary_sector)
   const monthText = monthsofyear.find((month) => month.id == user.month)
   
   if (secondary_sectorText) {
